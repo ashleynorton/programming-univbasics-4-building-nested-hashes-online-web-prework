7d64e61f4ea13a80408fbd57125b08ff85d44531
Ashley Norton <nortonashleym@gmail.com>
2019-08-13 01:15:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,8 +39,8 @@ def monopoly_with_third_tier
 	    :names => {
 	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
-	      :b_and_o_railroad => "name3",
-	      :name4 => "name4"
+	      :b_and_o_railroad => {},
+	      :name4 => {}
 	    }
 	  }
 	}
